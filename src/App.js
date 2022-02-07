b3556bcdc459ef43a3f2ba00754bb675ddcd0fa3
Vishal Ludhrani <vludrani@gmail.com>
2022-02-07 15:51:58 +0530

    chore: install tailwind, and prepare landing page (desktop)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+// import modules
+import React from 'react';
+
+// import styles, and/or components
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>Quick Poll</h1>
+      <div className="container mx-auto mt-40 space-y-8">
+        <h1 className="text-9xl font-bold">Quick Poll</h1>
+        <p className="text-4xl font-normal">A quick, open poll creator. View your poll results live. No limit on number of questions per poll. Create poll without signing in.</p>
+        <button className="bg-indigo-600 hover:bg-indigo-700 px-6 py-3 text-2xl rounded-full text-indigo-50">Create poll</button>
+      </div>
     </div>
   );
 }
